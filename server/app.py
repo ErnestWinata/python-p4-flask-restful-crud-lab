4cf0c98bc3a9c0683d9c637d8404baddfede7714
Ernest <ernestwinata@gmail.com>
2023-10-09 18:29:27 -0700

    initial commit

--- server/app.py
+++ server/app.py
@@ -17,6 +17,31 @@ db.init_app(app)
 api = Api(app)
 
 
+class PlantByID(Resource):
+
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first().to_dict()
+        return make_response(jsonify(plant), 200)
+
+    def patch(self, id):
+        data = request.get_json()
+        plant = Plant.query.get(id)
+
+        if plant:
+            plant.is_in_stock = data.get('is_in_stock', plant.is_in_stock)
+            db.session.commit()
+            return make_response(jsonify(plant.to_dict()), 200)
+        else:
+            return make_response(jsonify({'error': 'Plant not found'}), 404)
+        
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response('', 204)
+
+
 class Plants(Resource):
 
     def get(self):
@@ -38,18 +63,10 @@ class Plants(Resource):
         return make_response(new_plant.to_dict(), 201)
 
 
+
 api.add_resource(Plants, '/plants')
-
-
-class PlantByID(Resource):
-
-    def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
-
-
 api.add_resource(PlantByID, '/plants/<int:id>')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
